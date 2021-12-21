a8de73514e6be6ac2e4699dc9bd02a157f3180f1
moenah <moenah@hotmail.com>
2021-12-21 21:22:22 +0100

    orderseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             ProductSeeder::class,
             FirmSeeder::class,
+            OrderSeeder::class
 
         ]);
     }
